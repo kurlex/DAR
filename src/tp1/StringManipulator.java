af504e555e8439a4f3646f9278654a12c1766b55
Mohamed Aziz Bel Haj Amor <62076382+kurlex@users.noreply.github.com>
2021-11-22 23:16:03 +0100

    filling Functions

--- src/tp1/StringManipulator.java
+++ src/tp1/StringManipulator.java
@@ -1,4 +1,4 @@
-package tp1;
+//package tp1;
 
 public class StringManipulator {
 
@@ -9,28 +9,45 @@ public class StringManipulator {
     }
 
     public void reverse(){
-        //do your stuff
-        System.out.println("reverse : ");
+        String res = "";
+        for(int i = s.length()-1;i >= 0;i--)
+            res+=s.charAt(i);
+        System.out.println("reverse : "+res);
     }
 
     public void isPalindrome(){
         //do your stuff
-        System.out.println("isPalindrome : ");
+        int n = s.length();
+        for(int i = 0;i<n/2;i++)
+            if(s.charAt(i) != s.charAt(n-i-1)){
+                System.out.println("isPalindrome : False");
+                return;
+            }
+        System.out.println("isPalindrome : True");
     }
 
     public void toUpperCase(){
-        //do your stuff
-        System.out.println("uppercase : ");
+        String res ="";
+        for(int i=0;i<s.length();i++)
+            res+= Character.toUpperCase(s.charAt(i));
+        System.out.println("uppercase : "+res);
     }
 
     public void toLowerCase(){
-        //do your stuff
-        System.out.println("lowercase : ");
+        String res ="";
+        for(int i=0;i<s.length();i++)
+            res+= Character.toLowerCase(s.charAt(i));
+        System.out.println("lowercase : "+res);
     }
-
     public void getVowelNumber(){
-        //do your stuff
-        System.out.println("vowelNumber : ");
+        char []vowel = { 'A', 'E', 'I', 'O', 'U','Y'};
+        int con = 0;
+        for(int i=0;i < s.length();i++)
+            for(int j = 0;j < 6;j++)
+                if(Character.toUpperCase(s.charAt(i))== vowel[j] )
+                    con++;
+        
+        System.out.println("vowelNumber : "+con);
     }
 
     public static void main(String[] args) {
